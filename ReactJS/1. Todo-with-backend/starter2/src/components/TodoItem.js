df64425465df66dcc5f824e08cf05934ffb4363a
Loki343 <kausikbhaumik42@gmail.com>
2024-07-26 10:38:55 +0530

    kkk

---
+++ ReactJS/1. Todo-with-backend/starter2/src/components/TodoItem.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { toggleComplete } from '../redux/todoSlice';
+
+const TodoItem = ({ id, title, completed }) => {
+	const dispatch = useDispatch()
+	const handleComplete = () => {
+		dispatch(toggleComplete({
+			id: id,
+			completed: !completed
+		}))
+	}
+
+	return (
+		<li className={`list-group-item ${completed && 'list-group-item-success'}`}>
+			<div className='d-flex justify-content-between'>
+				<span className='d-flex align-items-center'>
+					<input type='checkbox' className='mr-3' checked={completed} onChange={handleComplete}></input>
+					<span className='m-3'>{title}</span>
+				</span>
+				<button className='btn btn-danger'>Delete</button>
+			</div>
+		</li>
+	);
+};
+
+export default TodoItem;
